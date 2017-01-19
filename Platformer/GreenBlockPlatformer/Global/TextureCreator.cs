5ac1649995b6c6539d7f75386f5157c95cc95907
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-01-19 23:27:20 +0100

    Added a TextureCreater so that textures can now be easily created

---
+++ Platformer/GreenBlockPlatformer/Global/TextureCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GreenBlockPlatformer.Global {
+    public static class TextureCreator {
+        public static Texture2D CreateMonoTexture(this GraphicsDevice device, int width, int height, Color color) {
+            Texture2D retTexture = new Texture2D(device, width, height);
+
+            Color[] colors = new Color[width * height];
+            for (int i = 0; i < colors.Length; i++) {
+                colors[i] = color;
+            }
+
+            retTexture.SetData(colors);
+
+            return retTexture;
+        }
+    }
+}
