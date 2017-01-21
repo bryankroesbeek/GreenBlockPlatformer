24708ba559035c14ecca2cab7951adb4dc3587d4
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-01-21 22:04:48 +0100

    Removed members from 'IBox' because they were hiding the members of 'IComponent'

--- Platformer/GreenBlockPlatformer/Objects/Character/IBox.cs
+++ Platformer/GreenBlockPlatformer/Objects/Character/IBox.cs
@@ -5,8 +5,6 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GreenBlockPlatformer.Objects.Character {
     public interface IBox : IComponent{
-        Texture2D Texture { get; set; }
-        Vector2 Position { get; set; }
         Vector2 Speed { get; set; }
         BoxState State { get; set; }
         Vector2 AirResistance { get; }
