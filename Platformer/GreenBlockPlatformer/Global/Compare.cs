d953cf927294b6073a8c82e182f676c151a8f765
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-01-28 10:17:59 +0100

    Added Compare static class for comparisons

---
+++ Platformer/GreenBlockPlatformer/Global/Compare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace GreenBlockPlatformer.Global {
+    public static class Compare {
+        public static bool IsBetween(this Vector2 point, Vector2 lowerBound, Vector2 upperBound) {
+            bool c1 = (point.X > lowerBound.X && point.Y > lowerBound.Y);
+            bool c2 = (point.X < upperBound.X && point.Y < upperBound.Y);
+
+            return c1 && c2;
+        }
+
+        public static bool IsBetween(this Vector2 point, Vector2 comparePoint, float offset) {
+            bool c1 = (point.X > comparePoint.X - offset && point.Y > comparePoint.Y - offset);
+            bool c2 = (point.X < comparePoint.X + offset && point.Y < comparePoint.Y + offset);
+
+            return c1 && c2;
+        }
+
+        public static bool IsBetween(this Vector2 point, Vector2 comparePoint, float offsetX, float offsetY) {
+            bool c1 = (point.X > comparePoint.X - offsetX && point.Y > comparePoint.Y - offsetY);
+            bool c2 = (point.X < comparePoint.X + offsetX && point.Y < comparePoint.Y + offsetY);
+
+            return c1 && c2;
+        }
+    }
+}
