5db5617756015f662c6a45c39dbcce967d912ad4
Bryan Kroesbeek <BryanK.B96@gmail.com>
2017-02-08 13:35:20 +0100

    Added method to create a Texture2D object from the given picture location

--- Platformer/GreenBlockPlatformer/Global/TextureCreator.cs
+++ Platformer/GreenBlockPlatformer/Global/TextureCreator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GreenBlockPlatformer.Global {
@@ -19,6 +20,10 @@ namespace GreenBlockPlatformer.Global {
             retTexture.SetData(colors);
 
             return retTexture;
+        }
+
+        public static Texture2D CreateTextureFromFile(this ContentManager content,string filename) {
+            return content.Load<Texture2D>(filename);
         }
     }
 }
